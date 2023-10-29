87af06ddb278ba45dbf8408315699078d44d789b
DyhaCreator <andropet228@gmail.com>
2023-10-29 10:01:00 -0400

    initial

---
+++ long_ariph.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+struct bigint{
+    const int len = 4;
+    int num[4];
+    bigint(){}
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "long_ariph.h"
+#define ll long long
+using namespace std;
+
+int main() {
+    cout << "HW" << endl;
+    return 0;
+}
